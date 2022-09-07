83587492b947a6256d002213983428d8d5a4fbf5
BadryHassan <68699881+BadryHassan@users.noreply.github.com>
2022-09-07 20:55:36 +0200

    initial commit

---
+++ app/graphConfig.js
@@ -0,0 +1,6 @@
+// Add here the endpoints for MS Graph API services you would like to use.
+const graphConfig = {
+  graphMeEndpoint: "https://graph.microsoft.com/v1.0/me",
+  graphMailEndpoint: "https://graph.microsoft.com/v1.0/me/messages",
+  graphGroupMembers: "https://graph.microsoft.com/v1.0/groups/4f7264b4-c650-4214-9c97-b5a3fc9b243c/members"
+};
